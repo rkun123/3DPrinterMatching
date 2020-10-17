8744481e815535b82bebe240b2f4c68dd80a10cf
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-17 13:48:26 +0900

    [update] startページの改良

--- vue/src/views/Start.vue
+++ vue/src/views/Start.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>Start Page</div>
+  <div class="start">
+    <img src="../assets/logo.png" />
+    <h1>3DPrinter Matching</h1>
+    <h2>description</h2>
+    <button @click="login">Login</button>
+    <button @click="signup">Signup</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    login() {
+      this.$router.push("/login");
+    },
+    signup() {
+      this.$router.push("/signup");
+    },
+  },
+};
 </script>
+
+<style scoped>
+button {
+  margin: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+</style>
