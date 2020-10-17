8b129b7538756ee2016e71ac209acbb4357b8b02
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-17 14:21:59 +0900

    [update] SignIn page

--- vue/src/views/SignUp.vue
+++ vue/src/views/SignUp.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>SignUp</div>
+  <div>
+    <form>
+      <p>username:</p>
+      <input v-model="username" />
+      <p>e-mail:</p>
+      <input v-model="email" autocomplete="email" />
+      <p>password:</p>
+      <input v-model="password1" type="password" />
+      <p>もう一度パスワードを入力してください</p>
+      <input v-model="password2" type="password" />
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form_user: {
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+      },
+    };
+  },
+};
 </script>
+
+<style scoped>
+</style>
