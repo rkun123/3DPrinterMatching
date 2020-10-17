e7d3f115dff496452b0aa8bbdcf541d535d51c29
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-17 12:44:43 +0900

    [add] page router

--- vue/src/views/Start.vue
+++ vue/src/views/Start.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div class="start">
     <img src="../assets/logo.png" />
     <h1>3DPrinter Matching</h1>
@@ -30,3 +31,11 @@ button {
   padding-right: 15px;
 }
 </style>
+=======
+  <div>Start Page</div>
+</template>
+
+<script>
+export default {};
+</script>
+>>>>>>> f092e2f... [add] page router
