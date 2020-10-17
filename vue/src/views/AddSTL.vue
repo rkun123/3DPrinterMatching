b0661db060944c0e121e7d1049b5fb9d127e30fe
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-18 02:17:36 +0900

    [add]GoogleMap

--- vue/src/views/AddSTL.vue
+++ vue/src/views/AddSTL.vue
@@ -1,56 +1,46 @@
 <template>
-    <div>
-        <form>
-            <p>name:</p>
-            <input v-model="form_data.name" />
-            <p>e-mail:</p>
-            <input v-model="form_data.stl" autocomplete="email" />
-        </form>
+  <div>
+    <form>
+      <p>name:</p>
+      <input v-model="form_data.name" />
+      <p>e-mail:</p>
+      <input v-model="form_data.stl" autocomplete="email" />
+    </form>
 
-        <button v-on:click="post">Greet</button>
-
-    </div>
+    <button v-on:click="post">Greet</button>
+  </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      form_data: {
+        name: "",
+        stl: null,
+        user: null,
+      },
+    };
+  },
+  methods: {
+    post: async function() {
+      const payload = JSON.stringify(this.form_data);
 
-    export default {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: payload,
+      };
 
-        data() {
-            return {
-                form_data: {
-                    "name": "",
-                    "stl": null,
-                    "user": null
-                }
-            }
-        },
-        methods: {
-            post: async function () {
-                // メソッド内の `this` は、 Vue インスタンスを参照します
-                alert('Hello ' + '!')
-
-                const payload = JSON.stringify(this.form_data)
-
-                const requestOptions = {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: payload
-                }
-
-                await fetch('http://127.0.0.1:8000/rest-auth/registration/', requestOptions)
-                    .then(response => response.json())
-                    .then(requestOptions => console.log(requestOptions))
-
-
-
-
-            }
-        },
-
-
-    }
+      await fetch(
+        "http://127.0.0.1:8000/rest-auth/registration/",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((requestOptions) => console.log(requestOptions));
+    },
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- vue/src/views/SignUp.vue
+++ vue/src/views/SignUp.vue
@@ -1,60 +1,63 @@
 <template>
-    <div>
-        <form>
-            <p>username:</p>
-            <input v-model="form_data.username" />
-            <p>e-mail:</p>
-            <input v-model="form_data.email" autocomplete="email" />
-            <p>password:</p>
-            <input v-model="form_data.password1" type="password" />
-            <p>もう一度パスワードを入力してください</p>
-            <input v-model="form_data.password2" type="password" />
-        </form>
+  <div>
+    <form>
+      <p>username:</p>
+      <input v-model="form_data.username" />
+      <p>e-mail:</p>
+      <input v-model="form_data.email" autocomplete="email" />
+      <p>password:</p>
+      <input v-model="form_data.password1" type="password" />
+      <p>もう一度パスワードを入力してください</p>
+      <input v-model="form_data.password2" type="password" />
+    </form>
 
-        <button v-on:click="post">Greet</button>
-
-    </div>
+    <GoogleMap @parentMethod="updatePosition" />
+    <button v-on:click="post">Greet</button>
+  </div>
 </template>
 
 <script>
-    export default {
+import GoogleMap from "../components/GoogleMap.vue";
 
-        data() {
-            return {
-                form_data: {
-                    username: '',
-                    email: '',
-                    password1: '',
-                    password2: '',
-                }
-            }
-        },
-        methods: {
-            post: async function () {
-                // メソッド内の `this` は、 Vue インスタンスを参照します
-                alert('Hello ' + '!')
+export default {
+  data() {
+    return {
+      form_data: {
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+        lat: 0,
+        lng: 0,
+      },
+    };
+  },
+  components: {
+    GoogleMap,
+  },
+  methods: {
+    post: async function () {
+      const payload = JSON.stringify(this.form_data);
 
-                const payload = JSON.stringify(this.form_data)
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: payload,
+      };
 
-                const requestOptions = {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: payload
-                }
-
-                await fetch('http://127.0.0.1:8000/rest-auth/registration/', requestOptions)
-                    .then(response => response.json())
-                    .then(requestOptions => console.log(requestOptions))
-
-
-
-
-            }
-        },
-
-
-    }
+      await fetch(
+        "http://127.0.0.1:8000/rest-auth/registration/",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((requestOptions) => console.log(requestOptions));
+    },
+    updatePosition(Position) {
+      this.lat = Position.lat;
+      this.lng = Position.lng;
+    },
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
