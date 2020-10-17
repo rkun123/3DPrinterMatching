7cb6f045c75e1474e510abe1bc7ee9a55108af3c
U3cSeroH1 <unkio0602@gmail.com>
2020-10-18 05:51:04 +0900

    [update]addSTL

--- vue/src/views/AddSTL.vue
+++ vue/src/views/AddSTL.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <form>
-      <p>name:</p>
-      <input v-model="form_data.name" />
-      <p>e-mail:</p>
-      <input v-model="form_data.stl" autocomplete="email" />
-    </form>
+      <form>
+          <p>name:</p>
+          <input v-model="form_data.name" />
+          <p>OMO</p>
+          <img :src="stl" alt="Avatar" class="image">
+          <label>
+              selectfile
+              <input type="file" @change="onFileChange" />
+          </label>
+      </form>
+
+
+
     <button v-on:click="post">Greet</button>
   </div>
 </template>
@@ -16,23 +23,40 @@ export default {
     return {
       form_data: {
         name: "",
-        stl: null,
-        user: null,
+        stl: '',
+        user: this.$store.state.UserState.pk,
       },
     };
   },
   methods: {
+      onFileChange(e) {
+          const files = e.target.files || e.dataTransfer.files;
+          this.createImage(files[0]);
+      },
+      // アップロードした画像を表示
+      createImage(file) {
+          const reader = new FileReader();
+          reader.onload = e => {
+              this.stl = e.target.result;
+          };
+
+          console.log(reader.readAsDataURL(file));
+      },
+
     post: async function () {
       const payload = JSON.stringify(this.form_data);
 
       const requestOptions = {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + this.$store.state.UserKey,
+        },
         body: payload,
       };
 
       await fetch(
-        "http://127.0.0.1:8000/rest-auth/registration/",
+        "http://127.0.0.1:8000/api/v1/object3d/",
         requestOptions
       )
         .then((response) => response.json())
