2c15e5554e085847c8005f274dadc19d6f7c53bc
U3cSeroH1 <unkio0602@gmail.com>
2020-10-17 15:17:40 +0900

    [add]ログイン　サンインイイ追加

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,85 +1,55 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    LoginPage<br />
-    <p>username</p>
-    <input v-model="form_data.username" placeholder="edit me" />
-    <p>e-mail</p>
-    <input v-model="form_data.email" placeholder="edit me" />
-    <p>password</p>
-    <input v-model="form_data.password" type="password" placeholder="edit me" />
-    <br />
-    <button v-on:click="post">Greet</button>
+    <div>
+        LoginPage<br />
+        <input v-model="form_data.username" placeholder="edit me">
+        <p>Message is: {{ form_data.username }}</p>
+        <input v-model="form_data.email" placeholder="edit me">
+        <p>Message is: {{ form_data.email }}</p>
+        <input v-model="form_data.password" type="password" placeholder="edit me">
+        <p>Message is: {{ form_data.password }}</p>
 
 
-    <p>{{key}}</p>
-  </div>
+        <button v-on:click="post">Greet</button>
+    </div>
 </template>
 
 <script>
+    export default {
+
+        data() {
+            return {
+                form_data: {
+                    username: '',
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            post: async function () {
+                // メソッド内の `this` は、 Vue インスタンスを参照します
+                alert('Hello ' + '!')
+
+                const payload = JSON.stringify(this.form_data)
+
+                const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: payload
+                }
+
+                await fetch('http://127.0.0.1:8000/rest-auth/login/', requestOptions)
+                    .then(response => response.json())
+                    .then(requestOptions => console.log(requestOptions))
+
+               
 
 
+            }
+        },
+          
+       
+    }
 
-export default {
-  data() {
-    return {
-      form_data: {
-        username: "",
-        email: "",
-        password: "",
-      },
-      key: "",
-    };
-  },
-  methods: {
-    post: async function() {
-      const payload = JSON.stringify(this.form_data);
-
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: payload,
-      };
-
-      await fetch("http://127.0.0.1:8000/rest-auth/login/", requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.key);
-            this.key = data.key;
-            this.$store.state.UserKey = data.key;
-        })
-        .then(() => {
-          //this.$router.push("/main");
-        })
-        .catch(() => {
-          alert("繝ｭ繧ｰ繧､繝ｳ縺ｫ螟ｱ謨励＠縺ｾ縺励◆");
-        });
-
-      const requestUser = {
-        method: "GET",
-        headers: { Authorization: "Token " + this.key },
-      };
-
-      await fetch("http://127.0.0.1:8000/rest-auth/user/", requestUser)
-        .then((response) => response.json())
-
-          .then((data) => {
-
-              this.key = data.pk;
-              this.$store.state.UserState = data;
-              console.log(this.$store.state.UserState);
-              console.log(this.$store.state.UserState.pk);
-          });
-
-
-    },
-  },
-};
-=======
-  <div>LoginPage</div>
-</template>
-
-<script>
-export default {};
->>>>>>> cfe6b66... [add] page router
-</script>
+    
+</script>  
