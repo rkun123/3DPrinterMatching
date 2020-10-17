044b455bae06db0c00e1d3359521cd5a7901ac0d
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-17 14:21:59 +0900

    [update] SignIn page

--- vue/src/views/SignUp.vue
+++ vue/src/views/SignUp.vue
@@ -1,69 +1,32 @@
 <template>
-<<<<<<< HEAD
-    <div>
-        <form>
-            <p>username:</p>
-            <input v-model="form_data.username" />
-            <p>e-mail:</p>
-            <input v-model="form_data.email" autocomplete="email" />
-            <p>password:</p>
-            <input v-model="form_data.password1" type="password" />
-            <p>もう一度パスワードを入力してください</p>
-            <input v-model="form_data.password2" type="password" />
-        </form>
-
-        <button v-on:click="post">Greet</button>
-
-    </div>
+  <div>
+    <form>
+      <p>username:</p>
+      <input v-model="username" />
+      <p>e-mail:</p>
+      <input v-model="email" autocomplete="email" />
+      <p>password:</p>
+      <input v-model="password1" type="password" />
+      <p>もう一度パスワードを入力してください</p>
+      <input v-model="password2" type="password" />
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-
-        data() {
-            return {
-                form_data: {
-                    username: '',
-                    email: '',
-                    password1: '',
-                    password2: '',
-                }
-            }
-        },
-        methods: {
-            post: async function () {
-                // メソッド内の `this` は、 Vue インスタンスを参照します
-                alert('Hello ' + '!')
-
-                const payload = JSON.stringify(this.form_data)
-
-                const requestOptions = {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: payload
-                }
-
-                await fetch('http://127.0.0.1:8000/rest-auth/registration/', requestOptions)
-                    .then(response => response.json())
-                    .then(requestOptions => console.log(requestOptions))
-
-
-
-
-            }
-        },
-
-
-    }
+export default {
+  data() {
+    return {
+      form_user: {
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
 </style>
-=======
-  <div>SignUp</div>
-</template>
-
-<script>
-export default {};
-</script>
->>>>>>> cfe6b66... [add] page router
