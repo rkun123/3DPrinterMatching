6539f4e939343ce2dc590610b87f30f2fb4fcee6
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-17 12:44:43 +0900

    [add] page router

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Start from '../views/Start.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import Main from '../views/UserMainpage.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Start',
+    component: Start
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main
   }
+
 ]
 
 const router = new VueRouter({
