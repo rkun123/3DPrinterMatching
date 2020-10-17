74378fb8edb891d2efc8b764deb538a15e98b0ea
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-10-17 12:44:43 +0900

    [add] page router

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>LoginPage</div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ vue/src/views/UserMainpage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>UserMainpage</div>
+</template>
+
+<script>
+export default {};
+</script>
